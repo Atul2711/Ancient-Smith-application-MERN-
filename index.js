29ae2a8d70f1477d684664d8937c9b87a70fe050
Shaurya111001 <adityasingh111001@gmail.com>
2022-09-04 15:01:01 +0530

    deploy4

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('my-app/build'));
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/my-app/build/index.html'));
+    res.sendFile(path.join(__dirname + './my-app/build/index.html'));
     });
    }
 
