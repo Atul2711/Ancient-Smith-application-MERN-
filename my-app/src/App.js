c52ff77a0fc01f4a2667c67b6263d8fa1c6ddd1d
Atul2711 <92645706+Atul2711@users.noreply.github.com>
2022-10-05 12:52:27 +0530

    Allpost added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ import RegisterPage from './pages/Register'
 import ProfilePage from './pages/Profile';
 import Post from './pages/Post'
 import ComposePage from './pages/Compose';
+import Allpost from './pages/Allpost';
 import { Context } from './context/Context';
 
 
@@ -25,6 +26,7 @@ function App() {
         <Route path='/profile' exact element={user?<ProfilePage/>:<LoginPage/>}></Route>
         <Route path="/compose" exact element={ user?<ComposePage/>: <LoginPage/>} />
         <Route path="/post/:postId" exact element={ <Post/> } />
+        <Route path="/sculpture" exact element={ <Allpost/> }/>
       </Routes>
 
     </Router>
